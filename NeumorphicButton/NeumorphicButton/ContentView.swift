51717be0b0e0ce2a77f18b9e58ca2a59192e32f8
Niraj Tiwari <tniraj7@gmail.com>
2020-05-10 15:35:57 +0900

    embed the button in ZStack and make background color of screen to light gray

--- NeumorphicButton/NeumorphicButton/ContentView.swift
+++ NeumorphicButton/NeumorphicButton/ContentView.swift
@@ -2,18 +2,23 @@ import SwiftUI
 
 struct ContentView: View {
     
-     @State private var showingAlert = false
+    @State private var showingAlert = false
     
     var body: some View {
         
+        ZStack {
+            Color(red: 224/255, green: 229/255, blue: 236/255)
             Button(action: {
                 self.showingAlert = true
             }) {
                 Text("Button")
             }
             .alert(isPresented: $showingAlert) {
-                Alert(title: Text("Message from Human Torch"), message: Text("Flame On !!"), dismissButton: .default(Text("OK")))
+                Alert(title: Text("Message from Human Torch"),
+                      message: Text("Flame On !!"),
+                      dismissButton: .default(Text("OK")))
             }
+        }
     }
 }
 
